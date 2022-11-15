65d7af1ff9ba5cfab6fb84038c2d9aef621a114e
Bitaw <stickynotes5@yahoo.com>
2022-11-15 00:10:49 -0600

    added get routes for '/', '/notes', and '/api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -2,4 +2,37 @@ const express = require('express')
 
 const app = express()
 
-app.listen(process.env.PORT || 3001)
+const path = require('path')
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+const notes = require('./db/db.json')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.post('/api/notes', (req, res) => {
+    let createdNote = {
+        title: req.body.title,
+        text: req.body.text,
+    }
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3001);
