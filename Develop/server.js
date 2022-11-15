e15768445f56018c3aebc3e45720b791bffd6205
Bitaw <stickynotes5@yahoo.com>
2022-11-15 01:40:43 -0600

    Added writeFile functionality to the post and delete routes for /api/notes in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,27 @@
-const { create } = require('domain')
-const express = require('express')
-const { uid } = require('uid')
-let notes = require('./db/db.json')
-
-const app = express()
-
-const path = require('path')
+const express = require('express');
+const { uid } = require('uid');
+let notes = require('./db/db.json');
+const app = express();
+const fs = require('fs');
+const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
@@ -32,13 +31,19 @@ app.post('/api/notes', (req, res) => {
         id: uid(),
     }
 
-    notes.push(createdNote);
-    res.json(200);
+    notes.push(createdNote)
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+        if (err) {console.log(err)}
+        res.json(createdNote);
+    });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    notes = notes.filter(notes => notes.id != req.params.id)
-    res.json(notes)
+    notes = notes.filter(note => note.id != req.params.id)
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+        if (err) {console.log(err)}
+        res.sendStatus(200);
+    })
 });
 
 
